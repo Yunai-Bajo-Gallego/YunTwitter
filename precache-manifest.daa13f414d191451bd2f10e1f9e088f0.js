4dd6604398b8c4f7a42320a19f1733d355ceb0ce
Yunai-Bajo-Gallego <yunaibg@hotmail.com>
2020-06-03 12:34:21 +0200

    Updates

---
+++ precache-manifest.daa13f414d191451bd2f10e1f9e088f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a5adf2539470668df2378614f897db",
+    "url": "/YunTwitter/index.html"
+  },
+  {
+    "revision": "86aa0107147273c2b565",
+    "url": "/YunTwitter/static/js/2.7d6f3065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YunTwitter/static/js/2.7d6f3065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbb0de7a1114b5f2058",
+    "url": "/YunTwitter/static/js/main.9f6c5b12.chunk.js"
+  },
+  {
+    "revision": "17821c0ce08b1ee84031",
+    "url": "/YunTwitter/static/js/runtime-main.1939b732.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YunTwitter/static/media/logo.5d5d9eef.svg"
+  }
+]);
